0a224151629f53eca6aa6fe12f7cb8a3a72ab8ac
Tatu Tarvainen <tatu.tarvainen@me.com>
2022-11-06 22:13:56 +0200

    Add some commands

--- redis.erl
+++ redis.erl
@@ -71,35 +71,61 @@ start(Port) ->
 serve(Table,Socket) ->
     receive
         {tcp, Socket, Data} ->
-            {ok, [Cmd | Args] = FullCmd, _} = read(Data),
-            io:format("GOT: ~p~nRAW: ~p~n", [FullCmd, Data]),
-            Res = try
-                      process(Table, Cmd, Args)
-                  catch
-                      throw:Msg -> {error, Msg};
-                      error:E ->
-                          io:format("ERROR ~p~n", [E]),
-                          {error, "Internal error, see log"}
-                  end,
-            io:format("  -> ~p~n", [Res]),
-            gen_tcp:send(Socket, write(Res)),
+            ok = process_cmds(Table, Data, Socket),
             serve(Table,Socket);
         {tcp_closed, Socket} ->
             ok
     end.
 
+process_cmds(Table, Data, Socket) ->
+    {ok, [Cmd | Args] = FullCmd, Rest} = read(Data),
+    %io:format("GOT: ~p~nRAW: ~p~n", [FullCmd, Data]),
+    Res = try
+              process(Table, Cmd, Args)
+          catch
+              throw:Msg -> {error, Msg};
+              error:E ->
+                  io:format("ERROR ~p~n", [E]),
+                  {error, "Internal error, see log"}
+          end,
+    %io:format("  -> ~p~nRAW> ~p~n", [Res, iolist_to_binary(write(Res))]),
+    gen_tcp:send(Socket, write(Res)),
+    if size(Rest) == 0 -> ok;
+       true -> process_cmds(Table, Rest, Socket)
+    end.
+
+coerce_int(Bin) ->
+    try binary_to_integer(Bin)
+    catch error:badarg -> throw("Not integer")
+    end.
+
+incr(T, Key, By) ->
+    Old = case  ets:lookup(T, Key) of
+              [{_,Val}] -> Val;
+              _ -> <<"0">>
+          end,
+    New = coerce_int(Old) + By,
+    ets:insert(T, {Key, integer_to_binary(New)}),
+    New.
+
 process(_, <<"COMMAND">>, [<<"DOCS">>]) -> "OK";
+%process(_, <<"PING">>, []) -> "PONG";
+%process(_, <<"PING">>, [Msg]) -> Msg;
+%process(_, <<"CONFIG">>, [Cmd, Name]) ->
+%    [Name,
+%     case [Cmd, Name] of
+%         [<<"GET">>, <<"save">>] -> "3600 1 300 100 60 10000";
+%         [<<"GET">>, <<"appendonly">>] -> "no"
+%     end];
 process(T, <<"SET">>, [Key, Val]) -> ets:insert(T, {Key,Val}), Val;
 process(T, <<"GET">>, [Key]) ->
     [{_,Val}] = ets:lookup(T, Key),
     Val;
 process(T, <<"INCR">>, [Key]) ->
-    Old = case  ets:lookup(T, Key) of
-              [{_,Val}] -> Val;
-              _ -> <<"0">>
-          end,
-    New = try binary_to_integer(Old) + 1
-          catch error:badarg -> throw("Not integer")
-          end,
-    ets:insert(T, {Key, integer_to_binary(New)}),
-    New.
+    incr(T, Key, 1);
+process(T, <<"INCRBY">>, [Key, By]) ->
+    incr(T, Key, coerce_int(By));
+process(T, <<"DECR">>, [Key]) ->
+    incr(T, Key, -1);
+process(T, <<"DECRBY">>, [Key, By]) ->
+    incr(T, Key, -1 * coerce_int(By)).
